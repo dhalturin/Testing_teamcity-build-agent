500c02bd03b501ddd11ef2ebccda5432603686c5
Denis Halturin <dhalturin@hotmail.com>
2020-03-29 07:56:01 +0300

    docker & ansible image

---
+++ packer/shell/docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -O - https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# apt-get update
+
+apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-compose
