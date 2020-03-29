c29c165f176c591763706ed8bf30040f2ac335d1
Denis Halturin <dhalturin@hotmail.com>
2020-03-29 07:52:39 +0300

    wrapper; env variables

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 source .env
 
-echo ${LINUX_ISO}
+PACKER_BIN=./packer/bin/packer
+FORCE=false
 
 msg_usage() {
     echo -e "Automate build images with TeamCity agents\n"
@@ -11,8 +12,10 @@ msg_usage() {
     echo -e "\t\$ ${0} -p\n"
     echo -e "Some of the options include:"
     echo -e "\t-b\tBuilding images. Arguments: all / linux / windows"
-    echo -e "\t-p\tPreparation before building images. Download packer"
+    echo -e "\t-c\tClean up. Shutdown vault server"
+    echo -e "\t-f\tUse this flag for rebuild images"
     echo -e "\t-h\tThis message"
+    echo -e "\t-p\tPreparation before building images. Download packer"
     exit
 }
 
@@ -36,19 +39,31 @@ build() {
         windows ) build_windows ;;
         *) msg_failure "Incorrect argument" ;;
     esac
+
+    cleanup
 }
 
 build_linux() {
     find ./packer/json/linux -type f | sort | while read json; do
-        echo "Validation json file: ${json}"
-        ./packer/bin/packer validate ${json}
+        IMAGE_PATH=$(jq -r '.variables.vm_output + "/" + .variables.vm_name' ${json})
 
-        if [ "${?}" -ne "0" ]; then
-            break
+        if [ "${FORCE}" = "true" ]; then
+            echo "Removing directory: ${IMAGE_PATH}"
+
+            rm -rfv ${IMAGE_PATH}
         fi
 
+        if [ -d ${IMAGE_PATH} ]; then
+            color_g "Skip file: ${json}, directory exists: ${IMAGE_PATH}"
+
+            continue
+        fi
+
+        echo "Validation json file: ${json}"
+        ${PACKER_BIN} validate ${json} || break
+
         echo "Building from json file: ${json}"
-        ./packer/bin/packer build ${json}
+        ${PACKER_BIN} build ${json} || break
     done
 }
 
@@ -77,18 +92,8 @@ check_command() {
     fi
 }
 
-prepare() {
-    check_command curl unzip
-
-    if [ ! -f ./packer/bin/packer ]; then
-        case $(uname -m) in
-            x86_64) ARCH=amd64 ;;
-            i386) ARCH=386 ;;
-            * ) msg_failure "Wrong architecture type" ;;
-        esac
-
-        OS=$(uname | tr '[:upper:]' '[:lower:]')
-
+download_packer() {
+    if [ ! -f ${PACKER_BIN} ]; then
         echo -n "Downloading packer.. "
 
         curl -so  /tmp/packer.zip https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_${OS}_${ARCH}.zip
@@ -109,15 +114,41 @@ prepare() {
     fi
 }
 
+prepare() {
+    check_command curl unzip jq VBoxManage
+
+    case $(uname -m) in
+        x86_64) ARCH=amd64 ;;
+        i386) ARCH=386 ;;
+        *) msg_failure "Wrong architecture type" ;;
+    esac
+
+    OS=$(uname | tr '[:upper:]' '[:lower:]')
+
+    download_packer
+}
+
+cleanup() {
+    if [ -f ./vault/.pid_file ]; then
+        cat ./vault/.pid_file | xargs kill
+    fi
+}
+
+set_force() {
+    FORCE=true
+}
+
 if [ -z "${*}" ]; then
     msg_usage
 fi
 
-while getopts ":b:ph" opt; do
+while getopts ":b:cfhp" opt; do
     case $opt in
         b) build ${OPTARG} ;;
-        p) prepare ;;
+        c) cleanup ;;
+        f) set_force ;;
         h) msg_usage ;;
+        p) prepare ;;
         *) msg_failure "An unknown argument was received" ;;
     esac
 done
