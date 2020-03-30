6e92aa6bd5b0b956ed63c99daa342772ece9ca47
Denis Halturin <dhalturin@hotmail.com>
2020-03-30 21:09:52 +0300

    prepare windows base

---
+++ packer/scripts/windows/prepare.ps1
@@ -0,0 +1,16 @@
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
+
+$ifaceinfo = Get-NetConnectionProfile
+Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private 
+
+winrm quickconfig -q
+winrm s "winrm/config" '@{MaxTimeoutms="1800000"}'
+winrm s "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
+winrm s "winrm/config/service" '@{AllowUnencrypted="true"}'
+winrm s "winrm/config/service/auth" '@{Basic="true"}'
+
+Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+
+sc.exe config winrm start= auto
+
+exit 0
